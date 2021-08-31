e23b46e438a81240ce35558eb32d77929b893901
Sueksit Vachirakumthorn <svac.mai@gmail.com>
2021-08-31 11:06:08 +0700

    master

--- 08/Leb 4/problem-4.c
+++ 08/Leb 4/problem-4.c
@@ -1,10 +1,26 @@
 #include<stdio.h>
+#include<math.h>
 
-int main()
-{
-    int num, x;
-    printf("Enter the number : ");
-    scanf("%d",&num);
-    printf("The factor of %d is ", num);    
-    return 0;
+int main(){
+	int n, i;
+	
+	printf("Enter the number: ");
+	scanf("%d",&n);
+	printf("The factor of %d is ",n);
+	
+	while (n%2 == 0){
+    	printf("%d*", 2);
+        n = n/2;
+    }
+
+    for (i=3; i <= sqrt(n); i=i+2){
+        while (n%i == 0){
+            printf("%d*" , i);
+            n = n/i;
+        }
+    }
+    if (n > 2){
+    	printf ("%d", n);
+	}
+	return 0;
 }
--- 08/Leb 4/rough.c
+++ 08/Leb 4/rough.c
@@ -2,5 +2,23 @@
 
 int main()
 {
+    int num, x;
+    printf("Enter the number : ");
+    scanf("%d",&num);
+    printf("The factor of %d is ", num);
+    while (num == 1)
+    {
+        switch (x)
+        {
+        case 'num%2 == 0':
+            printf("")
+            break;
+        
+        default:
+            break;
+        }
+    }
+        
+
     return 0;
 }
