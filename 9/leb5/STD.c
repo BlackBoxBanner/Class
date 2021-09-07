fff502ff3da81fe55f5839712d5af5836bcbf6bc
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2021-09-07 23:48:23 +0700

    master

--- 9/leb5/STD.c
+++ 9/leb5/STD.c
@@ -4,20 +4,20 @@
 
 int main()
 {
-    int counter;
+    int i;
     double sum = 0, mean, inb = 0, SD;
     double x[MAX] = {1,2,3,4,5,6,7,8,9,10};
 
-    for (counter = 0; counter < MAX; counter++) 
+    for (i = 0; i < MAX; i++) 
     {
-            sum += x[ counter ]; //find sum
+        sum += x[ i ]; //find sum
     }
 
     mean = sum/MAX;
 
-    for (counter = 0; counter < MAX; counter++) 
+    for (i = 0; i < MAX; i++) 
     {
-        inb += pow(x[counter]-mean,2); //find in the breaket^2
+        inb += pow(x[i]-mean,2); //find in the breaket^2
     }
     
     SD = (inb/MAX); //find SD
