03f7ae97c2f1ffcb84c2cebd339270419b7b1330
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2021-09-07 22:42:03 +0700

    master

--- 9/leb5/STD.c
+++ 9/leb5/STD.c
@@ -5,13 +5,23 @@
 int main()
 {
     int counter;
-    float sum, mean, inb, SD;
-    float x[MAX] = {1,2,3,4,5,6,7,8,9,10};
+    double sum = 0, mean, inb = 0, SD;
+    double x[MAX] = {1,2,3,4,5,6,7,8,9,10};
 
-    for (counter = 0; counter <= MAX; counter++) mean += x[ counter ]/MAX; //find sum
-    for (counter = 0; counter <= MAX; counter++) inb += (x[counter]-mean)*(x[counter]-mean); //find in the breaket^2
+    for (counter = 0; counter < MAX; counter++)
+    {
+            sum += x[ counter ]; //find sum
+    }
+
+    mean = sum/MAX;
+
+    for (counter = 0; counter < MAX; counter++) 
+    {
+        inb += pow(x[counter]-mean,2); //find in the breaket^2
+    }
+    
     SD = (inb/MAX); //find SD
 
-    printf("Standard Deviation is %.2f\n", sqrt(SD));
+    printf("Standard Deviation is %.2lf\n", sqrt(SD));
     return 0;
 }
